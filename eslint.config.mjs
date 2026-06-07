f7969750d26a5957bf469876ab54dba96e2c0ce8
Aryan Sharma <aryansharma24106@gmail.com>
2026-06-07 19:34:38 +0530

    chore: migrate configuration files and dependencies from nextjs to astro

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,14 @@
 import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
+  // Configure standard global ignores for Astro builds
   globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
+    ".astro/**",
+    "dist/**",
+    "node_modules/**",
     "out/**",
-    "build/**",
-    "next-env.d.ts",
   ]),
 ]);
 
 export default eslintConfig;
+
